53b32ef882019ef207b098bcd29ab6c487c3e429
Alumno <Alumno@ws204-11>
2018-08-22 18:02:07 -0700

    agregue comentario

--- src/Test1/Test.java
+++ src/Test1/Test.java
@@ -1,7 +1,8 @@
 package Test1;
-// windows - show view - others - git
+// windows - show view - others - git - Git repositories
 // Team - share project
-// Team - comit (para compartir local) 
+// Team - comit (para compartir local) - arrastrar a stage changes - agregar comentarios - dar en COMMIT 
+// en la ventana de Git repository dar en branches
 public class Test {
 
 	public static void main(String[] args) {
