8f99cc5aa303ee5a017fecbc4fcfb42ced1a4a6f
Alumno <Alumno@ws204-11>
2018-08-22 18:20:00 -0700

    Agregar nuevo atributo

--- src/Test1/Operaciones.java
+++ src/Test1/Operaciones.java
@@ -3,8 +3,22 @@ package Test1;
 /*
 Clase para crear constantes
 Puede crear metodos, dar valores
+No es necesario poner identificador de acceso al constructor 
+no se crea objetos en la clase ENUM 
 */
 
 public enum Operaciones {
-	SUMA,RESTA,MULTIPLICACION,DIVISION;
+	SUMA("+"),RESTA("-"),MULTIPLICACION("*"),DIVISION("/");
+	
+	private String valor;
+	
+	Operaciones(String valor){
+		this.valor = valor;
+		
+	}
+	
+	public String getValor(){
+		return valor;
+	}
 }
+
--- src/Test1/Test.java
+++ src/Test1/Test.java
@@ -8,6 +8,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Operaciones.MULTIPLICACION);
+		System.out.println(Operaciones.SUMA.getValor());
+		System.out.println(Operaciones.SUMA.name());
 	}
 
 }
