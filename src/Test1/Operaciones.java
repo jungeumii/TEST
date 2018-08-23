343bf05939246764495312182ab4f041b3c189f8
Alumno <Alumno@ws204-11>
2018-08-22 17:59:49 -0700

    primera actualizacion

---
+++ src/Test1/Operaciones.java
@@ -0,0 +1,10 @@
+package Test1;
+
+/*
+Clase para crear constantes
+Puede crear metodos, dar valores
+*/
+
+public enum Operaciones {
+	SUMA,RESTA,MULTIPLICACION,DIVISION;
+}
---
+++ src/Test1/Test.java
@@ -0,0 +1,12 @@
+package Test1;
+// windows - show view - others - git
+// Team - share project
+// Team - comit (para compartir local) 
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Operaciones.MULTIPLICACION);
+	}
+
+}
